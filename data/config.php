c34d313d0badc4792a96f5f070aa452657fd19c3
root <root@iZ28odav1zbZ.(none)>
2015-12-07 15:38:43 +0800

    add config.php file

---
+++ data/config.php
@@ -0,0 +1,37 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "db_mall_jszy";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "jjb225002";
+
+// table prefix
+$prefix    = "leb_";
+
+$timezone    = "Asia/Chongqing";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('Domain','http://mall.jszyoa.com');//定义域名
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','manage');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '2014-11-13 20:31:09');
+
+?>
